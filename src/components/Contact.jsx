0e3176568dfa8d73033d94869410604bb14f082d
jayanth penta <jayjayanth345@gmail.com>
2026-01-08 16:36:45 +0530

    Updated backend and contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,7 @@ export default function Contact() {
     setStatus("Sending...");
 
     try {
-      const res = await fetch("/api/contact", {
+      const res = await fetch("https://portfolio2-7i3g.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
